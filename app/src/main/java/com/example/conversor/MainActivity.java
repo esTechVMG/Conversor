a30f42006f078ca9a8684a6840de17ca07fe74c9
vicente_maroto_ESTECH <vicente.maroto@escuelaestech.es>
2020-03-03 11:27:16 +0100

    conversor implemented

--- app/src/main/java/com/example/conversor/MainActivity.java
+++ app/src/main/java/com/example/conversor/MainActivity.java
@@ -36,45 +36,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.convertButton:
-                out.setText(String.valueOf(getResult()));
+                if(input.getText().toString().isEmpty()){
+                    input.setText("0");
+                }
+                out.setText(getResult() + convertTo.getSelectedItem().toString());
+                break;
         }
     }
     public double getResult(){
-        double result=0;
-        try{
-            int num;
-            switch (convertFrom.getSelectedItemPosition()){
-                case 0:
-
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-            }
-            switch (convertTo.getSelectedItemPosition()){
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-            }
-            //double multiplier = getMultiplier();
-
-        }catch (Exception e){
-            Log.e("CONVERSOR","UNHANDLED EXCEPTION CONVERTING");
-            e.printStackTrace();
-        }
-        return result;
+        double result=Integer.valueOf(input.getText().toString());
+        double numOne=getMultiply(convertFrom.getSelectedItemPosition());
+        double numTwo=getDivide(convertTo.getSelectedItemPosition());
+        return result*numOne*numTwo;
     }
-    public double getMultiplier(int times){
+    public double getDivide(int option){
+        return 1/getMultiply(option);
+    }
+    public double getMultiply(int option){
+        switch (option){
+            case 0:
+                return 1000;
+            case 1:
+                return 1;
+            case 2:
+                return 0.1;
+            case 3:
+                return 0.01;
+            case 4:
+                return 0.001;
+            case 5://LEGUAS
+                return 4828.03;
+            case 6://MILLAS
+                return 1609.34;
+            case 7://ESTADIOS
+                return 201.168;
+            case 8://CADENAS
+                return 20.116676725;
+            case 9://BARRAS
+                return  5.029;
+            case 10://YARDAS
+                return 0.9144;
+            case 11://PIE
+                return 0.3048;
+            case 12://MANO
+                return 0.1016;
+            case 13://PULGADA
+                return 0.0254;
+            case 14://MILLAS NAUTICAS
+                return 1852;
+            case 15://BRAZAS
+                return 1.8288;
+            default:
+                return 0;
+        }
+    }
+    public double getMeterMultiplier(int times){
         boolean toUpOrDown=times>0;
         int num=1;
         for(int a=0;a!=Math.abs(times);){
